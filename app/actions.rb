91d6382f792a2e8e7aa68ae9afbdd7892bc1951e
cpintoval <cpintovalderrama@gmail.com>
2015-09-29 18:38:57 +0000

    Basic adding functionality implemented

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,20 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  Contact.all.to_json
+end
+
+post '/contacts/create' do
+  response = Hash.new
+  response[:result] = false
+  contact = Contact.new(firstname: params[:firstname], lastname: params[:lastname], email: params[:email])
+
+  if contact.save
+    response[:result] = true
+    response[:id] = contact.id
+  end
+
+  response.to_json
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,44 @@
-$(document).ready(function() {
+//This is an object literal which is going to hold all of my event handlers
+//and callback functions.
+var handlers = {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  getContacts: function() {
+    $.getJSON('/contacts', handlers.processContacts);
+  },
+
+  processContacts: function(data) {
+    var table = $('#contacts').find('tbody').empty();
+    $.each(data, function(index, contact) {
+      var tr = $('<tr>').appendTo(table);
+      $('<td>').text(contact.firstname).appendTo(tr);
+      $('<td>').text(contact.lastname).appendTo(tr);
+      $('<td>').text(contact.email).appendTo(tr);
+    });
+    $('#results').removeClass('hide');
+  },
+
+  addContact: function(event) {
+    // event.preventDefault();
+    var newContact = {
+      firstname: $('#firstname').val(),
+      lastname: $('#lastname').val(),
+      email: $('#email').val()
+    };
+    $.post('/contacts/create', newContact, handlers.addedContact, 'json');
+  },
+
+  addedContact: function(data) {
+    if (data.result) {
+      handlers.getContacts();
+    }
+    else {
+      alert("There was an error.");
+    }
+  }
+};
+
+// Function that assigns the button to its specific handler.
+$(function() {
+  $('#getContacts').on('click', handlers.getContacts);
+  $('#createContact').on('click', handlers.addContact);
 });
